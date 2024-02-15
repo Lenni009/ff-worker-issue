9f6289ecd32a1c6b50784e3d7ba2b3544465c7a0
Lenni <guentherhanslphdyt@gmail.com>
2024-02-15 12:16:34 +0000

    add build step

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import { compressImage } from './lib/main';
+
 const fileInput = document.getElementById('file-upload');
 
 fileInput?.addEventListener('change', async () => {
     if (!(fileInput instanceof HTMLInputElement)) return;
-    const files = Array.from(fileInput.files);
+    const files = Array.from(fileInput.files ?? []);
     const compressedFiles = files.map((file) => compressImage(file));
-    
+
     await Promise.all(compressedFiles);
     console.log('done', compressedFiles);
 })
