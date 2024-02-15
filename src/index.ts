05f996889bd0467a63a815054f79171b3fd60f38
Lenni <guentherhanslphdyt@gmail.com>
2024-02-15 12:48:13 +0000

    add guard clause against too low quality

--- src/index.ts
+++ src/index.ts
@@ -30,13 +30,13 @@ const maxSize = 500000; // 500KB
 
 export async function compressLoop(file: File, quality: number = 1): Promise<File> {
     if (file.size < maxSize) return file; // if below maxSize, don't do anything
-    console.log(quality)
     const res = await compressImage(file, {
-      quality,
-      type: imageTypes.JPEG,
+        quality,
+        type: imageTypes.JPEG,
     });
+    console.log(res.size, quality)
     const lowerQuality = quality - 0.01; // NoSonar reduce quality by 1%;
-    if (res.size > maxSize) return await compressLoop(file, lowerQuality); // compress original file with lower quality setting to avoid double compression
+    if (res.size > maxSize && quality > 0) return await compressLoop(file, lowerQuality); // compress original file with lower quality setting to avoid double compression
     const fileName = file.name.split('.').slice(0, -1).join('.');
     const newFileName = fileName + '-min.jpg';
     return new File([res], newFileName, { type: imageTypes.JPEG });
