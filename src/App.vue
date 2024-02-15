2aeff0d0b2a289400815d88d39aae2e3541047ae
Lenni009 <guentherhanslphdyt@gmail.com>
2024-02-15 17:39:17 +0100

    formatting

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import {computed, ref, watch} from 'vue';
-import {compressImage} from './lib/main';
+import { computed, ref, watch } from "vue";
+import { compressImage } from "./lib/main";
 interface FileObj {
-    id: number;
-    isCompressed: boolean;
-    file: File;
+  id: number;
+  isCompressed: boolean;
+  file: File;
 }
 
 const files = ref<FileObj[]>([]);
@@ -17,40 +17,57 @@ let id = 0;
  after compression, show files
 */
 
-
 async function addFiles() {
-    const fileList = Array.from(fileInput.value?.files ?? []);
+  const fileList = Array.from(fileInput.value?.files ?? []);
 
-    const fileObjects = fileList.map(file => ({id: id++, isCompressed: false, file}))
+  const fileObjects = fileList.map((file) => ({
+    id: id++,
+    isCompressed: false,
+    file,
+  }));
 
-    files.value.push(...fileObjects);
+  files.value.push(...fileObjects);
 }
 
-const uncompressedImages = computed(() => files.value.filter(item => !item.isCompressed));
+const uncompressedImages = computed(() =>
+  files.value.filter((item) => !item.isCompressed)
+);
 
-watch(() => files.value.length, () => {
+watch(
+  () => files.value.length,
+  () => {
     uncompressedImages.value.forEach(editFileObj);
-})
-
+  }
+);
 
 async function editFileObj(fileObj: FileObj) {
-    const compressedImage = await compressImage(fileObj.file);
+  const compressedImage = await compressImage(fileObj.file);
 
-    const compressedFile = new File([compressedImage], 'compressed', {type: 'image/jpeg'});
-    fileObj.isCompressed = true;
-    fileObj.file = compressedFile;
+  const compressedFile = new File([compressedImage], "compressed", {
+    type: "image/jpeg",
+  });
+  fileObj.isCompressed = true;
+  fileObj.file = compressedFile;
 }
 
 const allCompressed = computed(() => !uncompressedImages.value.length);
 </script>
 
 <template>
-    <input ref="fileInput" type="file" id="file-upload" multiple @change="addFiles">
-    
-    <p>Done? {{allCompressed}}</p>
+  <input
+    ref="fileInput"
+    type="file"
+    id="file-upload"
+    multiple
+    @change="addFiles"
+  />
 
-    <h2>Input files:</h2>
-    <ul>
-        <li v-for="file in files" :key="file.id">{{ file.file.name }}, {{file.file.size}}</li>
-    </ul>
+  <p>Done? {{ allCompressed }}</p>
+
+  <h2>Input files:</h2>
+  <ul>
+    <li v-for="file in files" :key="file.id">
+      {{ file.file.name }}, {{ file.file.size }}
+    </li>
+  </ul>
 </template>
